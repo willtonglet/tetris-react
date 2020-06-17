aadee853d86bdfd39d23cd3283a81ee2483528cc
William Tonglet <william.tonglet@wipro.com>
2020-06-16 21:42:06 -0300

    Updates

---
+++ precache-manifest.62c813d2f9dd65731cf8bac73b62c916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a927960f62242750bfcafb338ec290e",
+    "url": "/tetris-react/index.html"
+  },
+  {
+    "revision": "5d9c960a7bf3c7a00e96",
+    "url": "/tetris-react/static/css/main.c4951908.chunk.css"
+  },
+  {
+    "revision": "887cfe2c58a04edfcfdf",
+    "url": "/tetris-react/static/js/2.58112f6d.chunk.js"
+  },
+  {
+    "revision": "5d9c960a7bf3c7a00e96",
+    "url": "/tetris-react/static/js/main.ce994064.chunk.js"
+  },
+  {
+    "revision": "5ffb780af0f8d98f40e3",
+    "url": "/tetris-react/static/js/runtime~main.009b4986.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/tetris-react/static/media/Pixel-LCD-7.e2897552.woff"
+  }
+]);
